28c189b5e4ceb7b0217e38463eec607fddd68579
Alfonso Lozano Piedra <night.darkness_@outlook.com>
2024-04-19 21:36:19 -0600

    Debugging
    Downgrade python 3.12.2 > 3.11.9
    
    Reformateo de prints
    
    Fix para el puntero al salir de servers a episodios
    
    Ahora aparece el texto "Descargando" al abrir un servidor
    
    Fix para la finalizacion del programa

--- uwu-cli.py
+++ uwu-cli.py
@@ -32,6 +32,7 @@ class UwuCli():
             'yellow': '\033[93m',
             'blue': '\033[94m',
             'purple': '\033[95m',
+            'sub' : '\033[4m',
             'tick' : '\033[5m',
             'tick2' : '\033[6m',
             'inv' : '\033[7m',
@@ -45,15 +46,15 @@ class UwuCli():
         while True:
 
             os.system(self.clear_console)
-            print(f'{self.cmd_colors['tick2'] + self.cmd_colors['blue']}Name:{self.cmd_colors['end']} {self.cmd_colors['purple'] + anime.replace('-',' ') + self.cmd_colors['end']}\n')
+            print(f"{self.cmd_colors['tick2'] + self.cmd_colors['blue']}Name:{self.cmd_colors['end']} {self.cmd_colors['purple'] + anime.replace('-',' ') + self.cmd_colors['end']}\n")
 
             for i in range(episodes):
                 if self.position == i+1:
-                    print(f'[{self.cmd_colors['tick2'] + self.cmd_colors['inv']}*{self.cmd_colors['end']}] Episodio {str(i+1)}')
+                    print(f"[{self.cmd_colors['tick2'] + self.cmd_colors['inv']}*{self.cmd_colors['end']}] Episodio {str(i+1)}")
                 else:
-                    print(f'[ ] Episodio {str(i+1)}')
+                    print(f"[ ] Episodio {str(i+1)}")
             
-            print(f'{self.cmd_colors['yellow']}\nESC para salir...{self.cmd_colors['end']}')
+            print(f"{self.cmd_colors['yellow']}\nESC para salir...{self.cmd_colors['end']}")
 
             cursor = keyboard.read_key()
 
@@ -72,15 +73,15 @@ class UwuCli():
                 while True:
 
                     os.system(self.clear_console)
-                    print(f'{self.cmd_colors['tick2'] + self.cmd_colors['blue']}Name:{self.cmd_colors['end']} {self.cmd_colors['purple'] + anime.replace('-',' ')} Episodio {str(cap) + self.cmd_colors['end']}\n')
+                    print(f"{self.cmd_colors['tick2'] + self.cmd_colors['blue']}Name:{self.cmd_colors['end']} {self.cmd_colors['purple'] + anime.replace('-',' ')} Episodio {str(cap) + self.cmd_colors['end']}\n")
 
                     for i in range(servers):
                         if self.position == i+1:
-                            print(f'[{self.cmd_colors['tick2'] + self.cmd_colors['inv']}*{self.cmd_colors['end']}] {link[0][i]['server']}')
+                            print(f"[{self.cmd_colors['tick2'] + self.cmd_colors['inv']}*{self.cmd_colors['end']}] {link[0][i]['server']}")
                         else:
-                            print(f'[ ] {link[0][i]['server']}')
+                            print(f"[ ] {link[0][i]['server']}")
 
-                    print(f'{self.cmd_colors['yellow']}\nESC para volver...{self.cmd_colors['end']}')
+                    print(f"{self.cmd_colors['yellow']}\nESC para volver...{self.cmd_colors['end']}")
 
                     cursor = keyboard.read_key()
 
@@ -91,12 +92,13 @@ class UwuCli():
                         self.position -= 1
 
                     if cursor == 'enter' or cursor == 'space':
+                        print(f"\n\n{self.cmd_colors['green']}Descargando anime...{self.cmd_colors['end']}")
                         os.system('mpv ' + link[0][self.position-1]['code'])
                         break
 
                     if cursor == 'esc':
                         os.system(self.clear_console)
-                        print(f'Volviendo al menu anterior, por favor espera.')
+                        print(f"Volviendo al menu anterior, por favor espera.")
                         self.position = 1
                         self.contador = 0
                         cursor = None
@@ -137,19 +139,19 @@ class UwuCli():
                 os.system(self.clear_console)
                 temp = self.api.get_latest_episodes()
 
-                print(f'{self.cmd_colors['green']}Ultimos capitulos{self.cmd_colors['end']}\n')
+                print(f"{self.cmd_colors['green']}Ultimos capitulos{self.cmd_colors['end']}\n")
 
                 for i in temp:
-                    print(f'{i.anime.replace('-', ' ')} Episodio {i.id}')
+                    print(f"{i.anime.replace('-', ' ')} Episodio {i.id}")
 
             elif self.params[1] == 'n' or self.params[1] == 'N':
                 os.system(self.clear_console)
                 temp = self.api.get_latest_animes()
 
-                print(f'{self.cmd_colors['green']}Animes en emision{self.cmd_colors['end']}\n')
+                print(f"{self.cmd_colors['green']}Animes en emision{self.cmd_colors['end']}\n")
 
                 for i in temp:
-                    print(f'{i.title}')
+                    print(f"{i.title}")
 
         else:
             while True:
@@ -161,11 +163,11 @@ class UwuCli():
 
                     if self.contador < 3:
                         self.contador += 1
-                        print(f'{self.cmd_colors['yellow']}No hay respuesta del servidor...\n{self.cmd_colors['green']}Reintentando... (intento #{str(self.contador)}){self.cmd_colors['end']}')
+                        print(f"{self.cmd_colors['yellow']}No hay respuesta del servidor...\n{self.cmd_colors['green']}Reintentando... (intento #{str(self.contador)}){self.cmd_colors['end']}")
                         self.search_data = self.api.search(self.search)
                         time.sleep(0.5)
                     else:
-                        print(f'{self.cmd_colors['red']}No hay resultados para: "{self.cmd_colors['purple'] + self.search + self.cmd_colors['red']}"{self.cmd_colors['end']}')
+                        print(f"{self.cmd_colors['red']}No hay resultados para: \"{self.cmd_colors['purple'] + self.search + self.cmd_colors['red']}\"{self.cmd_colors['end']}")
                         sys.exit(0)
 
                 elif len(self.search_data) == 1:
@@ -174,7 +176,7 @@ class UwuCli():
                     episodes = self.search_data.episodes[0].id
                     self.viewer(anime, episodes)
                     break
-                
+
                 else:
                     self.position = 1
                     servers = len(self.search_data)
@@ -183,15 +185,15 @@ class UwuCli():
 
                         os.system(self.clear_console)
 
-                        print(f'Resultados para {self.cmd_colors['green'] + self.search.replace('-', ' ') + self.cmd_colors['end']}\n')
+                        print(f"Resultados para {self.cmd_colors['green'] + self.search.replace('-', ' ') + self.cmd_colors['end']}\n")
 
                         for i in range(servers):
                             if self.position == i+1:
-                                print(f'[{self.cmd_colors['tick2'] + self.cmd_colors['inv']}*{self.cmd_colors['end']}] {self.search_data[i].id.replace('-',' ')}')
+                                print(f"[{self.cmd_colors['tick2'] + self.cmd_colors['inv']}*{self.cmd_colors['end']}] {self.search_data[i].id.replace('-',' ')}")
                             else:
-                                print(f'[ ] {self.search_data[i].id.replace('-',' ')}')
+                                print(f"[ ] {self.search_data[i].id.replace('-',' ')}")
                         
-                        print(f'{self.cmd_colors['yellow']}\nESC para salir...{self.cmd_colors['end']}')
+                        print(f"{self.cmd_colors['yellow']}\nESC para salir...{self.cmd_colors['end']}")
 
                         cursor = keyboard.read_key()
 
